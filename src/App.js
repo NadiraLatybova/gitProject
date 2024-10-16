8a56fa09c506eb9e8b237978913e03b240942038
latybovanadira <nadiralatybova@gmail.com>
2024-10-16 17:28:53 +0600

    init push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import './App.css'
 
 function App() {
+
+  const [count,setCount] = useState(0)
+
+  function handleIncrement() {
+    setCount(prev => prev + 1)
+
+  }
+  function handleDecrement() {
+    setCount(prev => prev - 1)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+    <h1 className='heading'>{count}</h1>
+      <div className="buttons">
+        <button onClick={handleIncrement} className='button'>+</button>
+        <button onClick={handleDecrement} className='button'>-</button>
+      </div>
     </div>
   );
 }
