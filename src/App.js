fc034060533d1a2d7aa20931f7766c2442050461
shebrll <shebrll@icloud.com>
2024-10-18 17:06:18 +0600

    functions added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import {useState} from "react";
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
-  const [count,setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   function handleIncrement() {
-    setCount(prev => prev + 1)
-
+    setCount(prev => prev + 1);
   }
+
   function handleDecrement() {
-    setCount(prev => prev - 1)
-
+    setCount(prev => prev - 1);
   }
+
+  function handleMultiply() {
+    setCount(prev => prev * 2);
+  }
+
+  function handleDivide() {
+    setCount(prev => prev / 2);
+  }
+
   return (
-    <div className='wrapper'>
-    <h1 className='heading'>{count}</h1>
-      <div className="buttons">
-        <button onClick={handleIncrement} className='button'>+</button>
-        <button onClick={handleDecrement} className='button'>-</button>
-        <h2 className='heading2'>это каунтер</h2>
+      <div className="wrapper">
+        <h1 className="heading">{count}</h1>
+        <div className="buttons">
+          <button className="button" onClick={handleIncrement}>+</button>
+          <button className="button" onClick={handleDecrement}>-</button>
+          <button className="button" onClick={handleMultiply}>*</button>
+          <button className="button" onClick={handleDivide}>/</button>
+        </div>
+        <h2 className="heading2">Я БЭТМЕН</h2>
       </div>
-    </div>
   );
 }
 
